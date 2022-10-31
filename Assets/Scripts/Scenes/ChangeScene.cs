9783ecb929335c4b3310cd1aaa50698f821c4125
LuizH-Queiroz <luizh.keiroz@gmail.com>
2022-10-30 21:11:29 -0300

    Start Scene

---
+++ Assets/Scripts/Scenes/ChangeScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneToLoad;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Submit") == 1 && sceneToLoad != null)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
